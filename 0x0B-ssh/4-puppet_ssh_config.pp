f5eb33a2ead94f82b609542071c8cc9a6e7cbb9b
mounirchebbi <2157@holbertonschool.com>
2022-04-17 23:35:07 +0100

    puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# SSH client config file to connect to a server with RSA key.
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true
+}
+
